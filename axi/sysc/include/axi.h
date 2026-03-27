8d658d3e14eb61f3f54d0649e7159e9847b0086f
Abhay Pratap <abhay2110004@akgec.ac.in>
2026-03-27 16:28:43 +0000

    add about credits

--- axi/sysc/include/axi.h
+++ axi/sysc/include/axi.h
@@ -3,4 +3,14 @@
 class axi : public sc_module
 {
     axi(sc_core::sc_module_name a_name);
+
+    //Add implementation of Creadit support
+    int tx = 0;
+    int rx = RESET_CREADITS;
+    int RP = RP_NUM;
+    if ( tx == completed )
+    {
+        tx ++;
+        rx --;
+    }
 }
